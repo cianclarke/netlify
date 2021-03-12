163133c00c28188c513323680b63331138e04d98
cianclarke <cian@cianclarke.com>
2021-03-12 11:41:30 -0500

    Do fetch

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,25 @@
-exports.handler = async function(event, context) {
+const fetch = require('node-fetch')
+
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
     return {
-        statusCode: 200,
-        body: JSON.stringify(event)
-    };
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }
 }
