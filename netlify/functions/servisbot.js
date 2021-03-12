dc8094ee5b6ac87313f334b915afc67673b276ba
cianclarke <cian@cianclarke.com>
2021-03-12 17:44:14 -0500

    Servisbot

---
+++ netlify/functions/servisbot.js
@@ -0,0 +1,49 @@
+const fetch = require('node-fetch')
+
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
+
+exports.handler = async (ev, context) => {
+  const { Messages, CustomerReference } = ev;
+  Messages.map(async msg => {
+    let response
+    try {
+      response = await fetch('https://slack.com/api/chat.postMessage', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization' : `Bearer ${process.env.SLACK_KEY}`
+        },
+        body: JSON.stringify({
+          channel : CustomerReference,
+          text : msg
+        })
+      })
+      // handle response
+    } catch (err) {
+      console.log('err')
+      console.log(err);
+      return {
+        statusCode: err.statusCode || 500,
+        body: JSON.stringify({
+          error: err.message
+        })
+      }
+    }
+    const slackresponse = await response.json();
+    console.log('slack response')
+    console.log(slackresponse);
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({})
+  };
+  // 
+
+  
+  
+  
+  
+  
+  
+}
--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -28,63 +28,27 @@ exports.handler = async (ev, context) => {
   console.log(event);
   const utterance = text.replace(`<@${user}> `, '');
   
-  // await fetch(`https://engagement.eu-1.servisbot.com/picard/v1/engage/cianclarke-CiansWebhook`, {
-  //   method : 'POST',
-  //   headers : {
-  //     'Content-Type' : 'application/json',
-  //     'Authorization' : `Bearer ${process.env.SBOT_KEY}`
-  //   },
-  //   body : JSON.stringify({
-  //     "Type": "message", 
-  //     "Value": utterance, 
-  //     "CustomerReference": user
-  //   })
-  // });
+  await fetch(`https://engagement.eu-1.servisbot.com/picard/v1/engage/cianclarke-CiansWebhook`, {
+    method : 'POST',
+    headers : {
+      'Content-Type' : 'application/json',
+      'Authorization' : `Bearer ${process.env.SBOT_KEY}`
+    },
+    body : JSON.stringify({
+      "Type": "message", 
+      "Value": utterance, 
+      "CustomerReference": channel
+    })
+  });
+  
   console.log('sending tpoken')
   console.log({
     'Accept': 'application/json',
     'Content-Type': 'application/json',
     'Authorization' : `Bearer ${process.env.SLACK_KEY}`
   })
-   
-  let response
-  try {
-    response = await fetch('https://slack.com/api/chat.postMessage', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization' : `Bearer ${process.env.SLACK_KEY}`
-      },
-      body: JSON.stringify({
-        channel,
-        text : utterance
-      })
-    })
-    // handle response
-  } catch (err) {
-    console.log('err')
-    console.log(err);
-    return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message
-      })
-    }
-  }
-  const slackresponse = await response.json();
-  console.log('slack response')
-  console.log(slackresponse);
   return {
     statusCode: 200,
     body: JSON.stringify({})
   };
-  // 
-
-  
-  
-  
-  
-  
-  
 }
