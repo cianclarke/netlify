f5344f26b615c1cd3dbbbbdfd7fab4d5bd099cde
cianclarke <cian@cianclarke.com>
2021-03-12 11:24:03 -0500

    Strip graphql and stripe

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, StaticQuery } from 'gatsby'
+import { StaticQuery } from 'gatsby'
 import ProductCard from './ProductCard'
 
 const containerStyles = {
@@ -12,49 +12,7 @@ const containerStyles = {
 
 const Products = () => {
   return (
-    <StaticQuery
-      query={graphql`
-        query ProductPrices {
-          prices: allStripePrice(
-            filter: { active: { eq: true } }
-            sort: { fields: [unit_amount] }
-          ) {
-            edges {
-              node {
-                id
-                active
-                currency
-                unit_amount
-                product {
-                  id
-                  name
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={({ prices }) => {
-        // Group prices by product
-        const products = {}
-        for (const { node: price } of prices.edges) {
-          const product = price.product
-          if (!products[product.id]) {
-            products[product.id] = product
-            products[product.id].prices = []
-          }
-          products[product.id].prices.push(price)
-        }
-
-        return (
-          <div style={containerStyles}>
-            {Object.keys(products).map(key => (
-              <ProductCard key={products[key].id} product={products[key]} />
-            ))}
-          </div>
-        )
-      }}
-    />
+    <h1>Whatever</h1>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
@@ -9,46 +9,7 @@ import stripeLogo from '../images/powered_by_stripe.svg'
 import '@stripe/stripe-js' // https://github.com/stripe/stripe-js#import-as-a-side-effect
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            <div>
-              Built by <a href="https://twitter.com/thorwebdev">Thor</a> with{' '}
-              <a href="https://www.gatsbyjs.org">Gatsby</a> | View{' '}
-              <a href="https://github.com/gatsbyjs/gatsby/tree/master/examples/ecommerce-tutorial-with-stripe">
-                source
-              </a>
-            </div>
-            <div>
-              <a href="https://stripe.com">
-                <img src={stripeLogo} alt="Payments powered by Stripe" />
-              </a>
-            </div>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+  <h1>Hi</h1>
 )
 
 Layout.propTypes = {
