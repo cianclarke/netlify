17ca379e5bcedfced0bf851f101d0caf930cabda
cianclarke <cian@cianclarke.com>
2021-03-12 17:13:12 -0500

    Less logs

--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -4,8 +4,6 @@ const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
 
 exports.handler = async (ev, context) => {
   let body;
-  console.log('In the function')
-  console.log(ev)
   if (!ev.body){
     console.log('No body')
     return {
@@ -30,22 +28,30 @@ exports.handler = async (ev, context) => {
   console.log(event);
   const utterance = text.replace(`<@${user}> `, '');
   
+  // await fetch(`https://engagement.eu-1.servisbot.com/picard/v1/engage/cianclarke-CiansWebhook`, {
+  //   method : 'POST',
+  //   headers : {
+  //     'Content-Type' : 'application/json',
+  //     'Authorization' : 'Bearer xoxb-32731804422-1878588355600-Sjdz2WGDgPdf33LGCuJ9JI1c'
+  //   },
+  //   body : JSON.stringify({
+  //     "Type": "message", 
+  //     "Value": utterance, 
+  //     "CustomerReference": user
+  //   })
+  // });
+  
+  
   let response
   try {
-    console.log('responding with')
-    console.log({
-      token : process.env.SLACK_KEY,
-      channel,
-      text : utterance
-    })
     response = await fetch('https://slack.com/api/chat.postMessage', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization' : process.env.SLACK_KEY
       },
       body: JSON.stringify({
-        token : process.env.SLACK_KEY,
         channel,
         text : utterance
       })
