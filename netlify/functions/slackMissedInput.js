98c7a04eea44098b67bf0573242cf3fdc9ed34bb
cianclarke <cian@cianclarke.com>
2021-09-08 15:58:31 -0400

    Add blocks

--- netlify/functions/slackMissedInput.js
+++ netlify/functions/slackMissedInput.js
@@ -31,7 +31,61 @@ exports.handler = async (ev, context) => {
       },
       body: JSON.stringify({
         channel : 'ciansbottest',
-        text : `New missed input: ${userInput}`
+        text : `New missed input: ${userInput}`,
+        blocks : {
+                    "type": "actions",
+                    "block_id": "actions1",
+                    "elements": [
+                      {
+                        "type": "static_select",
+                        "placeholder":{
+                            "type": "plain_text",
+                            "text": "Which witch is the witchiest witch?"
+                        },
+                        "action_id": "select_2",
+                        "options": [
+                          {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Matilda"
+                            },
+                            "value": "matilda"
+                          },
+                          {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Glinda"
+                            },
+                            "value": "glinda"
+                          },
+                          {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Granny Weatherwax"
+                            },
+                            "value": "grannyWeatherwax"
+                          },
+                          {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Hermione"
+                            },
+                            "value": "hermione"
+                          }
+                        ]
+                      },
+                      {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Cancel"
+                        },
+                        "value": "cancel",
+                        "action_id": "button_1"
+                      }
+                    ]
+                  }
+
       })
     })
     // handle response
