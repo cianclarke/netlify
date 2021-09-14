c411a1bb7ee0a0c324f120cf571d0b18d54d6bc1
cianclarke <cian@cianclarke.com>
2021-09-14 14:59:38 -0400

    SS fn

---
+++ netlify/functions/securesession.js
@@ -0,0 +1,20 @@
+exports.handler = async (event, context) => {
+  const tokens = ['1a', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'];
+  console.log('event');
+  console.log(event);
+  console.log('context');
+  console.log(context);
+  if (event.body.token in tokens || event.queryStringParameters.token in tokens){
+    return {
+      statusCode: 200,
+      body: JSON.stringify(event.body)
+    }
+  }
+  
+  return {
+    statusCode: 401,
+    body: JSON.stringify({
+      event, context
+    })
+  }
+}
