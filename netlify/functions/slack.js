1bae070c3dd957a0a4c05155fe257206c8d1fdf0
cianclarke <cian@cianclarke.com>
2021-09-16 15:26:02 -0400

    Handle actions

--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -13,8 +13,12 @@ exports.handler = async (ev, context) => {
   }
   
   console.log(ev.body);
+  if (ev.body.startsWith('payload=')){
+    ev.body.replace('payload=', '');
+    ev.body = decodeURIComponent(ev.body);
+  }
   body = JSON.parse(ev.body);
-  const { challenge, event } = body;
+  const { challenge, event, type, actions } = body;
   if (!event && challenge){
     return {
       statusCode: 200,
@@ -23,6 +27,15 @@ exports.handler = async (ev, context) => {
       })
     };
   };
+  
+  if (type === 'block_actions'){
+    console.log('value');
+    console.log(actions[0].value);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({})
+    };
+  }
   
   const { channel, text, user } = event;
   console.log('Gots event:')
