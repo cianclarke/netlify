a304dc1457b00edfd8c6b8c98b70ece81dbb4daa
cianclarke <cian@cianclarke.com>
2021-11-01 14:32:07 -0400

    try catch

--- netlify/functions/twiml.js
+++ netlify/functions/twiml.js
@@ -1,7 +1,12 @@
 
 
 exports.handler = async (event, context) => {
-  let body = event.body && JSON.parse(event.body);
+  try{
+    let body = event.body && JSON.parse(event.body);
+  }catch(err){
+    console.log('JSON parse error');
+    console.log(err);
+  }
   body = body || {
     buildingName : 'unknown',
     branchCallback : 'unknown',
@@ -23,7 +28,7 @@ exports.handler = async (event, context) => {
     },
     body: `
 <Response>
-  <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at ${buildingName} Someone has pushed the emergency call button in elevator number ${elevatorNumber} but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at ${branchCallback}.</Say>
+  <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at ${buildingName}. Someone has pushed the emergency call button in elevator number ${elevatorNumber} but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at ${branchCallback}.</Say>
   <Gather input="speech dtmf" timeout="3" numDigits="1">
       <Say>Please press 1 or say yes to acknowledge. Press 2 to pass this message to the next contact.</Say>
   </Gather>
