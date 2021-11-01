017ffe34557c3eea25114659ab097f6c144c8525
cianclarke <cian@cianclarke.com>
2021-11-01 14:41:05 -0400

    Only gather

--- netlify/functions/twiml.js
+++ netlify/functions/twiml.js
@@ -19,10 +19,7 @@ exports.handler = async (event, context) => {
     branchCallback,
     elevatorNumber
   } = body;
-  console.log('event');
-  console.log(event);
-  console.log('context');
-  console.log(context);
+  // <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at ${buildingName}. Someone has pushed the emergency call button in elevator number ${elevatorNumber} but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at ${branchCallback}.</Say>
   return {
     statusCode: 200,
     headers : {
@@ -30,7 +27,7 @@ exports.handler = async (event, context) => {
     },
     body: `
 <Response>
-  <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at ${buildingName}. Someone has pushed the emergency call button in elevator number ${elevatorNumber} but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at ${branchCallback}.</Say>
+  
   <Gather input="speech dtmf" timeout="3" numDigits="1">
       <Say>Please press 1 or say yes to acknowledge. Press 2 to pass this message to the next contact.</Say>
   </Gather>
