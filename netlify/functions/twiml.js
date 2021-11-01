c187b357da25000c76601967c70e613e13f4aea1
cianclarke <cian@cianclarke.com>
2021-11-01 14:28:03 -0400

    Add placeholders

--- netlify/functions/twiml.js
+++ netlify/functions/twiml.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
     },
     body: `
 <Response>
-  <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at _(building name)_. Someone has pushed the emergency call button in elevator # __ but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at _(branch callback #)_.</Say>
+  <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at ${buildingName} Someone has pushed the emergency call button in elevator number ${elevatorNumber} but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at ${branchCallback}.</Say>
   <Gather input="speech dtmf" timeout="3" numDigits="1">
       <Say>Please press 1 or say yes to acknowledge. Press 2 to pass this message to the next contact.</Say>
   </Gather>
