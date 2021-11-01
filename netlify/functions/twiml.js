38f07cd0694494e88c5e0f0d37a9e8c1514e5e31
cianclarke <cian@cianclarke.com>
2021-11-01 14:21:01 -0400

    update twiml

--- netlify/functions/twiml.js
+++ netlify/functions/twiml.js
@@ -10,8 +10,13 @@ exports.handler = async (event, context) => {
     },
     body: `
 <Response>
-  <Say voice="alice">Thanks for trying our documentation. Enjoy!</Say>
-  <Play>http://demo.twilio.com/docs/classic.mp3</Play>
+  <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at _(building name)_. Someone has pushed the emergency call button in elevator # __ but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at _(branch callback #)_.</Say>
+  <Response>
+    <Gather input="speech dtmf" timeout="3" numDigits="1">
+        <Say>Please press 1 or say yes to acknowledge. Press 2 to pass this message to the next contact.</Say>
+    </Gather>
+  </Response>
+
 </Response>
 `
   }
