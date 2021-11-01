5afef7e44bd9a36b1bbf478be2ed3ebcb1120d8c
cianclarke <cian@cianclarke.com>
2021-11-01 13:48:50 -0400

    Move init

--- netlify/functions/twilio.js
+++ netlify/functions/twilio.js
@@ -1,11 +1,12 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
 
 exports.handler = async (event, context) => {
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require('twilio')(accountSid, authToken);
+
   // conversation start
   client.calls
         .create({
