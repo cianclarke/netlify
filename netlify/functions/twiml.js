e9cc83b37cc6a59faa979092950012fe915cec66
cianclarke <cian@cianclarke.com>
2021-11-01 14:25:33 -0400

    Adapt twiml

--- netlify/functions/twiml.js
+++ netlify/functions/twiml.js
@@ -1,4 +1,17 @@
+
+
 exports.handler = async (event, context) => {
+  let body = event.body && JSON.parse(event.body);
+  body = body || {
+    buildingName : 'unknown',
+    branchCallback : 'unknown',
+    elevatorNumber : 'unknown'
+  };
+  const { 
+    buildingName, 
+    branchCallback,
+    elevatorNumber
+  } = body;
   console.log('event');
   console.log(event);
   console.log('context');
@@ -11,12 +24,9 @@ exports.handler = async (event, context) => {
     body: `
 <Response>
   <Say voice="alice">Elevator Emergency: This is an automated message from TKE. We monitor your elevators at _(building name)_. Someone has pushed the emergency call button in elevator # __ but there is no response or indication of a problem. Could you please have someone check the elevator to make sure no one is trapped or injured inside? If you need further assistance please call us back at _(branch callback #)_.</Say>
-  <Response>
-    <Gather input="speech dtmf" timeout="3" numDigits="1">
-        <Say>Please press 1 or say yes to acknowledge. Press 2 to pass this message to the next contact.</Say>
-    </Gather>
-  </Response>
-
+  <Gather input="speech dtmf" timeout="3" numDigits="1">
+      <Say>Please press 1 or say yes to acknowledge. Press 2 to pass this message to the next contact.</Say>
+  </Gather>
 </Response>
 `
   }
