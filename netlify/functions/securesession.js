ccabbbf441d97e589592b3ed733b89c6aab1582c
cianclarke <cian@cianclarke.com>
2021-11-08 19:48:01 -0500

    no spread

--- netlify/functions/securesession.js
+++ netlify/functions/securesession.js
@@ -8,14 +8,15 @@ if (!timeout || !expired){
 
 exports.handler = async (event, context) => {
   const tokens = ['1a', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'];
+  const token = event.body.token || event.queryStringParameters.token;
   console.log('event');
   console.log(event);
   console.log('context');
   console.log(context);
-  if (tokens.includes(event.body.token) || tokens.includes(event.queryStringParameters.token)){
+  if (tokens.includes(token)){
     return {
       statusCode: 200,
-      body: { expired, ...event.body }
+      body: { expired, token }
     }
   }
   
